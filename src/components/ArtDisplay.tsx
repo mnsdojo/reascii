2c71c933f5e06d41803fb08f0df104f5d6f7b899
notageek-guy <manishkumar97430@gmail.com>
2024-06-03 23:57:42 +0530

    added the resizing based on fontsize

--- src/components/ArtDisplay.tsx
+++ src/components/ArtDisplay.tsx
@@ -1,16 +1,13 @@
-interface Props {
-  art: string;
-  croppedArt: string;
-}
-
-function ArtDisplay({ art, croppedArt }: Props) {
+function ArtDisplay({ art, size }: { art: string; size: number }) {
   return (
     <div className="flex items-center justify-center">
-      {art ? (
-        <pre>{croppedArt}</pre>
-      ) : (
-        <p className="font-medium">Paste your art here by "Ctrl+C"</p>
-      )}
+      <div style={{ fontSize: size }}>
+        {art ? (
+          <pre>{art}</pre>
+        ) : (
+          <p className="font-medium">Paste your art here by "Ctrl+C"</p>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/ResizeControls.tsx
+++ src/components/ResizeControls.tsx
@@ -1,40 +1,16 @@
 interface ResizeControlsProps {
-  height: number;
-  width: number;
-  handleHeightChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleWidthChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleIncrement: () => void;
+  handleDecrement: () => void;
 }
 
 function ResizeControls({
-  height,
-  width,
-  handleHeightChange,
-  handleWidthChange,
+  handleDecrement,
+  handleIncrement,
 }: ResizeControlsProps) {
   return (
     <div className="absolute bottom-8 left-8 gap-4 flex">
-      <div className="flex flex-col items-center">
-        <h2>Height</h2>
-        <input
-          type="range"
-          min="1"
-          max="50"
-          value={height}
-          onChange={handleHeightChange}
-        />
-        <span>{height}</span>
-      </div>
-      <div className="flex flex-col items-center">
-        <h2>Width</h2>
-        <input
-          type="range"
-          min="1"
-          max="100"
-          value={width}
-          onChange={handleWidthChange}
-        />
-        <span>{width}</span>
-      </div>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
     </div>
   );
 }
